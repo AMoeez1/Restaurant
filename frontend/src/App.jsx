5e5bbeb0f02b12da3fd7413da9cbca3d1e2e9328
AMoeez1 <mujjeequraishi@gmail.com>
2025-06-07 22:21:24 +0500

    error solved for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Router from "./Router";
-import "react-toastify/dist/ReactToastify.css";
+import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from "react-toastify";
 
 function App() {
